277be3c0c5e51023850f77d9a51bd8888cf18ce7
Mandeep <msj1990@gmail.com>
2025-05-19 18:43:41 +0100

    first commit

---
+++ main.js
@@ -0,0 +1,12 @@
+//I'm going to send a message to the user to prompt them to enter their name and date of birth
+
+console.log('Hello dear user, tell me your name and your date of birth, not including the year. eg. Mandeep January 12')
+
+//This collects the relavent data from the arguments the user puts in when running the program
+const userName = process.argv[2];
+const month = process.argv[3];
+const date = process.argv[4];
+
+
+
+
