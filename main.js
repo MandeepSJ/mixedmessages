b56b883098d041da8b3412d5ee4315a54cbe7e9c
Mandeep <msj1990@gmail.com>
2025-05-19 19:07:16 +0100

    start to write function for assigning star sign

--- main.js
+++ main.js
@@ -5,7 +5,23 @@ console.log('Hello dear user, tell me your name and your date of birth, not incl
 //This collects the relavent data from the arguments the user puts in when running the program
 const userName = process.argv[2];
 const month = process.argv[3];
-const date = process.argv[4];
+const day = process.argv[4];
+
+//The code below figures out the horoscope of the user and logs another message to the console
+let starSign = 'nil'
+
+function defineStarSign(month, day) {
+    if(month === 'January') {
+    if(day < 20) {
+        return starSign = 'Capricorn';
+    } else {
+        return starSign = 'Aquarius';
+    }
+}
+};
+
+defineStarSign(month, day);
+console.log(`${starSign}`);
 
 
 
